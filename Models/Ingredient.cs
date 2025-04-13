5829f31bf5a08cc191ac2fa6a37954376ada25bc
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2025-04-12 23:08:51 -0300

    defining ingredient class

---
+++ Models/Ingredient.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace digital_menu.Models
+{
+    public class Ingredient
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Unit { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+        
+    }
+}
