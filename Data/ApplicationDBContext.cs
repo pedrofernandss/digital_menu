db5d8ae6e7a65d18b15dd6d279099139659beef0
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2025-04-14 20:16:42 -0300

    refining data relations

--- Data/ApplicationDBContext.cs
+++ Data/ApplicationDBContext.cs
@@ -16,5 +16,22 @@ namespace digital_menu.Data
         public DbSet<Recipe> Recipes {get; set;}
         public DbSet<Ingredient> Ingredients {get; set;}
         public DbSet<RecipeIngredient> RecipeIngredients {get; set;}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<RecipeIngredient>()
+                .HasKey(ri => new { ri.RecipeId, ri.IngredientId });
+
+            modelBuilder.Entity<RecipeIngredient>()
+                .HasOne(ri => ri.Recipe)
+                .WithMany(r => r.RecipeIngredients)
+                .HasForeignKey(ri => ri.RecipeId);
+
+            modelBuilder.Entity<RecipeIngredient>()
+                .HasOne(ri => ri.Ingredient)
+                .WithMany(i => i.RecipeIngredients)
+                .HasForeignKey(ri => ri.IngredientId);
+        }
     }
 }
