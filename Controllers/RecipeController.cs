45f93944f12d5e9edd0daf612edfa3eb9c6c8627
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2025-04-14 21:06:46 -0300

    defining recipe controllers

---
+++ Controllers/RecipeController.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using digital_menu.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace digital_menu.Controllers
+{
+    [Route("api/recipes")]
+    [ApiController]
+    public class RecipeController : ControllerBase
+    {
+        private readonly ApplicationDBContext _context;
+        public RecipeController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll() {
+            var recipes = _context.Recipes.ToList();
+            return Ok(recipes);
+        }
+
+        [HttpGet("id")]
+        public IActionResult GetById([FromRoute] int id) {
+            var recipe = _context.Recipes.Find(id);
+
+            if(recipe == null) {
+                return NotFound();
+            }
+
+            return Ok(recipe);
+        }
+        
+    }
+}
