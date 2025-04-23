bc88a658721a64d1f3b0d63fb61c5cf5a73adb6f
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2025-04-23 19:03:38 -0300

    add post endpoint for recipe

--- Controllers/RecipeController.cs
+++ Controllers/RecipeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using digital_menu.Data;
+using digital_menu.Dtos.Recipe;
+using digital_menu.Mappers;
 using Microsoft.AspNetCore.Mvc;
 
 namespace digital_menu.Controllers
@@ -34,5 +36,13 @@ namespace digital_menu.Controllers
             return Ok(recipe);
         }
         
+        [HttpPost]
+        public IActionResult Create([FromBody] CreateRecipeRequestDto recipeDto) 
+        {
+            var recipeModel = recipeDto.ToRecipeFromCreateDTO();
+            _context.Recipes.Add(recipeModel);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(GetById), new {id = recipeModel}, recipeModel.ToRecipeDto());
+        }
     }
 }
