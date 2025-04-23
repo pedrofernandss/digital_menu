1c9c6651350068663b3f5ae2bcb1bf7ffd2ea339
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2025-04-23 17:20:31 -0300

    creating ingredient mappers

---
+++ Mappers/IngredientMappers.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using digital_menu.Dtos.Ingredient;
+using digital_menu.Models;
+
+namespace digital_menu.Mappers
+{
+    public static class IngredientMappers
+    {
+        public static IngredientDto ToIngredientDto(this Ingredient ingredientModel)
+        {
+            return new IngredientDto
+            {
+                Id = ingredientModel.Id,
+                Name = ingredientModel.Name,
+                Unit = ingredientModel.Unit,
+            };
+        }
+    }
+}
