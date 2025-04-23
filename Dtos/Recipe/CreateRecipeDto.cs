5b55cc2462b60a0fe4319474d6809ea8405fae02
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2025-04-23 19:03:11 -0300

    creating dto to recipe post requests

---
+++ Dtos/Recipe/CreateRecipeDto.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace digital_menu.Dtos.Recipe
+{
+    public class CreateRecipeRequestDto
+    {
+        public string Name { get; set; } = string.Empty;
+        public string PreparationMethod { get; set; } = string.Empty;
+    }
+}
