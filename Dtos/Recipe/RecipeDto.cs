5cca2603978747f8dc6650db4c6a785348b396ec
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2025-04-24 16:21:14 -0300

    add ingredients to recipe dto

--- Dtos/Recipe/RecipeDto.cs
+++ Dtos/Recipe/RecipeDto.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using digital_menu.Dtos.RecipeIngredient;
 
 namespace digital_menu.Dtos.Recipe
 {
@@ -10,6 +11,8 @@ namespace digital_menu.Dtos.Recipe
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string PreparationMethod { get; set; } = string.Empty;
+
+        public List<RecipeIngredientDto> Ingredients { get; set; } = new();
         
     }
 }
