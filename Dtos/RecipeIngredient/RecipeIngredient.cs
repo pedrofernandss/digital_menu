90592782306ddfe2b1ef4bd70f510536b5c3d081
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2025-04-24 16:22:42 -0300

    dto to get recipeingredient dto

--- Dtos/RecipeIngredient/RecipeIngredient.cs
+++ Dtos/RecipeIngredient/RecipeIngredient.cs
@@ -6,13 +6,13 @@ using digital_menu.Models;
 
 namespace digital_menu.Dtos.RecipeIngredient
 {
-    public class RecipeIngredient
+    public class RecipeIngredientDto
     {
         public int RecipeId { get; set; }
-        public RecipeIngredient Recipe { get; set; } = null!;
+        public string Recipe { get; set; } = null!;
 
         public int IngredientId { get; set; }
-        public RecipeIngredient Ingredient { get; set; } = null!;
+        public string Ingredient { get; set; } = null!;
 
         public decimal Quantity { get; set; }
     }
