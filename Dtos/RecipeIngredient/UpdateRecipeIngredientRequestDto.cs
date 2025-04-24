b183cb5e472b3646c323cff212df6379bcea3d2c
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2025-04-24 16:22:55 -0300

    dto to update recipeingredient

---
+++ Dtos/RecipeIngredient/UpdateRecipeIngredientRequestDto.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace digital_menu.Dtos.RecipeIngredient
+{
+    public class UpdateRecipeIngredientRequestDto
+    {
+        public decimal Quantity { get; set; }
+    }
+}
